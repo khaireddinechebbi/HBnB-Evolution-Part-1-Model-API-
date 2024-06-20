fcc3d0b13cd3ba39512c8c5a842d8f892bf6f5bd
essioui <salahessioui@gmail.com>
2024-06-20 18:28:19 +0100

    documentation

--- main_app.py
+++ main_app.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-"""Main application for integrating all APIs"""
+"""
+Main application for integrating all APIs
+
+Modules included:
+- api_user: Manages user-related endpoints.
+- api_country: Manages country-related endpoints.
+- api_city: Manages city-related endpoints.
+- api_amenity: Manages amenity-related endpoints.
+- api_place: Manages place-related endpoints.
+- api_review: Manages review-related endpoints.
+
+Usage:
+    Run this script directly to start the Flask application.
+    The application will run in debug mode.
+"""
 
 import os
 import sys
 from flask import Flask
 from flask_restx import Api
 
-# Adding the path to the modules
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'api')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'data')))
 
-# Importing individual API modules
 from api.api_user import ns as user_namespace
 from api.api_country import ns as country_namespace
 from api.api_city import ns as city_namespace
@@ -21,7 +33,6 @@ from api.api_review import ns as review_namespace
 app = Flask(__name__)
 api = Api(app)
 
-# Adding namespaces to the API
 api.add_namespace(user_namespace, path='/users')
 api.add_namespace(country_namespace, path='/countries')
 api.add_namespace(city_namespace, path='/cities')
